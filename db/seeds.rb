dab280e0dcfbf91cf57ad2a694a1b16b1793cc8e
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 15:06:51 -0500

    fixed helper name

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,11 @@ end
 40.times do
   user_id = (1..10).to_a
   post_id = (1..20).to_a
+  
   Comment.create(content: Faker::Lorem.sentence(word_count = 4), post_id: post_id.sample, user_id: user_id.sample)
 end
 
 20.times do
   user_id = (1..10).to_a
-  Post.create(title: Faker::Lorem.sentence(word_count = 4), user_id: user_id.sample)
+  Post.create(title: Faker::Lorem.sentence(word_count = 4), user_id: user_id.sample, content: Faker::Lorem.sentence(word_count = 9))
 end
